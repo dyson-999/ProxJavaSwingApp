361618ac34fe8f580f74cc2d9024005596e389cc
nimath123 <sa24610665@my.sliit.lk>
2025-05-14 22:32:08 +0530

    Add files via upload

---
+++ src/ProxJavaSwingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.proxjavaswingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxJavaSwingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxJavaSwingAppApplication.class, args);
+    }
+
+}
