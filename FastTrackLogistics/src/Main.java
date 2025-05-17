21a987296bafc9a22770e57372a659028fbf6bac
Gayan_Kavishka <sa24610422@my.sliit.lk>
2025-05-17 17:15:03 +0530

    Set up the database connection

--- FastTrackLogistics/src/Main.java
+++ FastTrackLogistics/src/Main.java
@@ -1,15 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import utils.DBConnection;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Testing FastTrack Logistics Database Connection...");
+        try (Connection conn = DBConnection.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM shipments")) {
+            System.out.println("Successfully connected to MySQL database!");
+            System.out.println("Fetching data from shipments table:");
+            while (rs.next()) {
+                int id = rs.getInt("shipment_id");
+                String senderName = rs.getString("sender_name");
+                String receiverName = rs.getString("receiver_name");
+                String status = rs.getString("status");
+                System.out.printf("Shipment ID: %d, Sender: %s, Receiver: %s, Status: %s%n",
+                        id, senderName, receiverName, status);
+            }
+        } catch (SQLException e) {
+            System.err.println("Database connection failed: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
---
+++ FastTrackLogistics/src/utils/DBConnection.java
@@ -0,0 +1,24 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/fasttrack_logistics?useSSL=false";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    static {
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
